3f83e51e1216096a440522ec708f97415e16e467
Andrew Kind <andrew.w.kind@gmail.com>
2020-08-17 14:01:56 -0600

    Add calculator php file.

---
+++ techcareerscalculator.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: TECHCareers Calculator
+Plugin URI: https://TECHCareers.ca/
+Description: An example plugin for the TECHCareers class. It provides an example of a WordPress shortcode. Type [techcareerscalculator] into a page or post to output our calculator.
+Author: Warren Uhrich
+Version: 1.0
+Author URI: https://WarrenUhrich.ca
+*/
